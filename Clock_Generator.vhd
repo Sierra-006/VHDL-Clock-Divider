683d8007c399cdc9dcb60bdcb4f3ea14d7e78c6a
Sierra-006 <hassan.mex.dxb@gmail.com>
2024-10-06 20:41:20 +0200

    Update Clock_Generator.vhd

--- Clock_Generator.vhd
+++ Clock_Generator.vhd
@@ -1,30 +1,3 @@
---LIBRARY IEEE;
---USE IEEE.STD_LOGIC_1164.ALL;
-
---entity Clock_Generator is
---    port (
---        clk : in std_logic;
---        clk_out : out std_logic
---    );
---end Clock_Generator;
-
---architecture Behavioral of Clock_Generator is
---    signal count : integer := 0;
---    signal temp : std_logic := '0';
---begin
---    process(clk) 
---    begin
---        if rising_edge(clk) then
---            count <= count + 1;
---            if count = 125 then
---                temp <= not temp;
---                count <= 0;
---            end if;
---        end if;
---    end process;
-
---    clk_out <= temp;
---end Behavioral;
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.ALL;
